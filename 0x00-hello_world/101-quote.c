116d705a6325627424e1b62351e79725a0ee4150
NatiGirma <natigirma.code@gmail.com>
2022-12-12 10:27:56 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
 * main - Entry point
-* main - followed by new line to the standard error
+* 
 * Return: Always 1 (Success)
 */
 int main(void)
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
