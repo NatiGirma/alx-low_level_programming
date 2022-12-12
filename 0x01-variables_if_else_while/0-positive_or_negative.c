defe0ffbb1ea4b581f9fda5f19c0eba212c59e3a
NatiGirma <natigirma.code@gmail.com>
2022-12-12 12:06:45 -0800

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+int a = 0;
+if (a<0){
+printf("%d is negative ");
+else
+printf("%d is positive\n", a);
+return (0);
+}
