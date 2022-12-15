193ea6733e5b3e8737b5808a455f8f077c7882d6
NatiGirma <natigirma.code@gmail.com>
2022-12-15 12:49:43 -0800

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @n: as int 
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar (95);
+	}
+putchar('\n');
+}
