3d668f7aaa5e8c824035d6b57e489fe26169e7e6
NatiGirma <natigirma.code@gmail.com>
2022-12-16 13:31:51 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - function to print a triangle
+ *
+ * @size: as int
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n);
+	}
+	_putchar('\n');
+}
